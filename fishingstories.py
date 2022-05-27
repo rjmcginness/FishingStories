e70dbe825e85f84012b89cd1880f74d718b01f2f
Robert J. McGinness <robertjmcginness5@gmail.com>
2022-05-26 22:45:03 -0400

    Add files via upload

--- fishingstories.py
+++ fishingstories.py
@@ -13,6 +13,7 @@ from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
 from sqlalchemy import String
+from sqlalchemy import Numeric
 
 from sqlalchemy.orm import relationship
 
@@ -23,9 +24,11 @@ Base = declarative_base()
 account_priviledges = Table('account_priviledge',
                             Base.metadata,
                             Column('account_type_id',
-                                   ForeignKey('account_type.id')),
+                                   ForeignKey('account_type.id'),
+                                   primary_key=True),
                             Column('priviledge_id',
-                                   ForeignKey('priviledge.id'))
+                                   ForeignKey('priviledge.id'),
+                                   primary_key=True)
                             )
 
 class Priviledge(Base):
@@ -34,3 +37,21 @@ class Priviledge(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True)
     account_types = relationship('AccountType', secondary=account_priviledges)
+
+class AccountType(Base):
+    __tablename__ = 'account_types'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    price = Column(Numeric, nullable=False)
+    account_types = relationship('Priviledge', secondary=account_priviledges)
+
+class UserAccount(Base):
+    __tablename__ = 'user_accounts'
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String(30), nullable=False, unique=True)
+    password = Column(String(20), nullable=False)
+    account_type_id = Column(Integer, ForeignKey('account_type.id'))
+    angler_id = Column(Integer, ForeignKey('angler,id'))
+    
