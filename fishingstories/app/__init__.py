b438e6e687aac14d119fa5031a609cd9be0a695c
Robert J. McGinness <robertjmcginness5@gmail.com>
2022-06-01 15:47:16 -0400

    Add files via upload

---
+++ fishingstories/app/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  1 14:48:24 2022
+
+@author: Robert J McGinness
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+from app import routes
--- fishingstories/app/routes.py
+++ fishingstories/app/routes.py
@@ -1,1 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  1 14:53:05 2022
 
+@author: Robert J McGinness
+"""
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
