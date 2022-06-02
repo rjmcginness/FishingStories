f34e9d56c377e34b9e5e63c85f5fc2b8ef0fb399
Robert J. McGinness <robertjmcginness5@gmail.com>
2022-06-02 16:04:58 -0400

    Add files via upload

---
+++ fishingstories/config.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  2 13:32:32 2022
+
+@author: Robert J McGinness
+"""
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'bron-girl'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
