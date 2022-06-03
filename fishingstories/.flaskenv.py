9d157824ce84f19d4da306d570b181408a907891
Robert J. McGinness <robertjmcginness5@gmail.com>
2022-06-03 01:34:42 -0400

    Delete fishingstories directory

--- fishingstories/.flaskenv.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun  1 15:30:03 2022
-
-@author: Robert J McGinness
-"""
-
-FLASK_APP=fishingstories.py
--- fishingstories/fishingstories.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun  1 15:12:01 2022
-
-@author: Robert J McGinness
-"""
-
-from app import app
