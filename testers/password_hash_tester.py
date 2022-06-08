8bd44012c200c03b4c962f341fff5b481ff815f3
Robert J. McGinness <robertjmcginness5@gmail.com>
2022-06-08 15:28:25 -0400

    Add files via upload

--- testers/password_hash_tester.py
+++ testers/password_hash_tester.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  8 15:23:49 2022
 
+@author: rmcginness
+"""
+
+from werkzeug.security import generate_password_hash
+
+hash_ = generate_password_hash('bronwyn')
+
+print(hash_)
