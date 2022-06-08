8b2b5606cbeb64fcc66d540afae23b442928bc83
Robert J. McGinness <robertjmcginness5@gmail.com>
2022-06-08 15:46:36 -0400

    Add files via upload

--- testers/password_hash_tester.py
+++ testers/password_hash_tester.py
@@ -6,7 +6,11 @@ Created on Wed Jun  8 15:23:49 2022
 """
 
 from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
 
 hash_ = generate_password_hash('bronwyn')
 
 print(hash_)
+
+print('CHECK PASSWORD:', check_password_hash(hash_, 'bronwyn'))
+print('CHECK PASSWORD:', check_password_hash(hash_, 'Bronwyn'))
