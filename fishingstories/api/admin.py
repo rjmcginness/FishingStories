44ee9e912f6e0bae5f88b838dfc18e6039c0c002
Robert J. McGinness <robertjmcginness5@gmail.com>
2022-06-14 00:36:09 -0400

    Add files via upload

--- fishingstories/api/admin.py
+++ fishingstories/api/admin.py
@@ -9,6 +9,7 @@ from flask import current_app
 from flask import Blueprint
 from flask import render_template
 from flask import flash
+from flask_login import login_required
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 
@@ -21,21 +22,25 @@ bp = Blueprint('admin', __name__)
 
 
 @bp.route('/admin')
+@login_required
 def admin():
-    return render_template('admin/index.html')
+    return render_template('admin/index.html', authenticated=True)
 
 
 @bp.route('/manage-anglers')
+@login_required
 def manage_anglers():
-    return render_template('admin/anglermenu.html')
+    return render_template('admin/anglermenu.html', authenticated=True)
 
 @bp.route('/anglers')
+@login_required
 def anglers():
     anglers_list = current_app.session.execute(select(models.Angler))
     
-    return render_template('admin/anglers.html', anglers=anglers_list)
+    return render_template('admin/anglers.html', anglers=anglers_list, authenticated=True)
 
 @bp.route('/ranks', methods = ['GET', 'POST'])
+@login_required
 def ranks():
     form = RankForm()
     ranks = current_app.session.execute(select(models.Rank))
@@ -57,11 +62,12 @@ def ranks():
             
     form.clear()
             
-    return render_template('admin/ranks.html', form=form, ranks=ranks)
+    return render_template('admin/ranks.html', form=form, ranks=ranks, authenticated=True)
 
             
 
 @bp.route('/create-anglers')
+@login_required
 def add_angler():
     form = CreateAnglerForm()
     if form.validate_on_submit():
