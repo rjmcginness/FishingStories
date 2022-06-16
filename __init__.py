6d4a984f31bad322b5899b8aa26182d43ebabb7f
Robert J. McGinness <robertjmcginness5@gmail.com>
2022-06-16 01:39:53 -0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 15 21:07:30 2022
+
+@author: Robert J McGinness
+"""
+
+# from sys import path
+# print('PATH:', path)
+# from pathlib import Path
+# import os
+
+
+# # set PYTHONPATH
+# def set_paths():
+#     base_path = os.fspath(Path(os.path.abspath('../')))
+    
+#     if base_path not in path:
+#         path.append(base_path)
+    
+#     if base_path + '/src' not in path:
+#         path.append(base_path + '/src')
