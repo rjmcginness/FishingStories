18a99bcadaf25324d2ffb42fb9d5a3d0fb853170
Robert J. McGinness <robertjmcginness5@gmail.com>
2022-06-17 02:39:07 -0400

    Delete src directory

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 15 21:51:46 2022
-
-@author: Robert J McGinness
-"""
-
--- src/nature/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 16 01:44:32 2022
-
-@author: Robert J McGinness
-"""
-
