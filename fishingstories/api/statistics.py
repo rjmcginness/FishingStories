4093df37f3fadba71dbf0f20e6594bacdf5b65f9
Robert J. McGinness <robertjmcginness5@gmail.com>
2022-06-27 17:54:11 -0400

    Add files via upload

--- fishingstories/api/statistics.py
+++ fishingstories/api/statistics.py
@@ -12,6 +12,7 @@ from flask import flash
 from flask_login import login_required
 
 from sqlalchemy import select
+from sqlalchemy import text
 
 
 from dataclasses import dataclass
@@ -49,45 +50,49 @@ def statistics(angler_id: int):
     sb.total_fish = list(current_app.session.execute(stmt).scalars())[0]
     
     # number of each species caught
-    stmt = f'''
-            SELECT species, COUNT(*) FROM fishes
-            WHERE angler_id = {angler_id}
-            GROUP BY species
-            ORDER BY COUNT(*) DESC;
-            '''
+    stmt = text(f'''
+                SELECT species, COUNT(*) FROM fishes
+                WHERE angler_id = {angler_id}
+                GROUP BY species
+                ORDER BY COUNT(*) DESC;
+                ''')
            
     sb.catch_count_by_species = list(current_app.session.execute(stmt))
     
     # Get Average Weight and Length of fish by species
-    stmt = f'''
-            SELECT species, AVG(weight), AVG(length) FROM fishes
-            WHERE angler_id = {angler_id}
-            GROUP BY species;
-            '''
+    stmt = text(f'''
+                SELECT species, AVG(weight), AVG(length) FROM fishes
+                WHERE angler_id = {angler_id}
+                GROUP BY species;
+                ''')
            
-    sb. species_avg_weight_length = list(current_app.session.execute(stmt))
+    # sb.species_avg_weight_length = list(current_app.session.execute(stmt))
+    sb.species_avg_weight_length = list(current_app.session.execute(stmt))
+    sb.species_avg_weight_length = [(s[0], '{: .2f} lbs'.format(s[1]) if s[1] is not None else '0.0lb',
+                                     '{: .2f}"'.format(s[2]) if s[2] is not None else '0.0"') for s in sb.species_avg_weight_length]
+    
     
     # number of fish caught at each spot
-    stmt = f'''
-            SELECT s.name, COUNT(f.id) FROM fishes f
-            INNER JOIN fishing_spots s
-            ON f.fishing_spot_id = s.id
-            WHERE angler_id = {angler_id}
-            GROUP BY s.name;
-            '''
+    stmt = text(f'''
+                SELECT s.name as spot_name, COUNT(f.id) FROM fishes f
+                INNER JOIN fishing_spots s
+                ON f.fishing_spot_id = s.id
+                WHERE f.angler_id = {angler_id}
+                GROUP BY s.name;
+                ''')
     
     sb.fish_at_spots = list(current_app.session.execute(stmt))
     
-    stmt = f'''
-            WITH fish_counts as (
-            SELECT s.name as spot_name, COUNT(f.id) as fish_count FROM fishes f
-            INNER JOIN fishing_spots s
-            ON f.fishing_spot_id = s.id
-            WHERE angler_id = {angler_id}
-            GROUP BY s.name
-            ) SELECT spot_name, MAX(fish_count) FROM fish_counts
-            GROUP BY spot_name;
-            '''
+    stmt = text(f'''
+                WITH fish_counts as (
+                SELECT s.name as spot_name, COUNT(f.id) as fish_count FROM fishes f
+                INNER JOIN fishing_spots s
+                ON f.fishing_spot_id = s.id
+                WHERE angler_id = {angler_id}
+                GROUP BY s.name
+                ) SELECT spot_name, MAX(fish_count) FROM fish_counts
+                GROUP BY spot_name;
+                ''')
             
     sb.best_spots = list(current_app.session.execute(stmt))
     
