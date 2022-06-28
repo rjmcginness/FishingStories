b80de17ea7e49d8b397c762f55723e8eec662d8b
Robert J. McGinness <robertjmcginness5@gmail.com>
2022-06-28 02:18:37 -0400

    Add files via upload

--- fishingstories_install.sh
+++ fishingstories_install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo -n "Enter full path for the directory for FishingStories."
-read directory
-cd $directory
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
--- fishingstories_install_win.bat
+++ fishingstories_install_win.bat
@@ -1,10 +1,8 @@
 @ECHO OFF
-set /p directory="Enter the full path to the directory for FishingStories."
-cd directory
 python -m venv .venv
-.venv\Scripts\activate
-pip install -r requirements
+.venv/bin/activate
+pip install -r requirements.txt
 
 docker compose up -d
 cd data
-cat fishing_stories.sql | docker exec -i fishing_container psql
+type fishing_stories.sql | docker exec -i fishing_container psql
